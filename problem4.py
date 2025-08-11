9fcc90b4ed21bdc10e14ce0cc3fcf3726b76aec9
coder kartik <singhkartik9142@gmail.com>
2025-08-11 20:37:52 +0530

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,9 @@
+# Numbering limit
+
+username = input("Enter your username :")
+
+if(len(username)<=5):
+    print ("valid username")
+else:
+    print("invalid username")
+    print("username must be 5 characters")
